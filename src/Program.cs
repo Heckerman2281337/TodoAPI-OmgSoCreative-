fd90dba68de744a2c081667416dc29d477b2094f
Heckerman2281337 <maskskozevnikov4@gmail.com>
2026-08-06 17:32:34 +0300

    Fix for deployment

--- src/Program.cs
+++ src/Program.cs
@@ -7,7 +7,7 @@ using Serilog.Events;
 
 public class Program
 {
-    public static async void Main(string[] args)
+    public static async Task Main(string[] args)
     {
 
         Log.Logger = new LoggerConfiguration()
